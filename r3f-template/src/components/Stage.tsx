3fab1be82945d7c306d7cd6cc49de649a0cf1e45
Siegfried Schaefer <siegfried.schaefer@gmail.com>
2024-12-15 15:17:32 +0100

    Added Cube

--- r3f-template/src/components/Stage.tsx
+++ r3f-template/src/components/Stage.tsx
@@ -1,14 +1,13 @@
 
 import { OrbitControls } from "@react-three/drei";
+import {Cube} from "./Cube";
 
 export const Stage = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={0.5} />
+      <Cube />
     </>
   );
 };
